50e7fe316631aee51d365a9fc8aec3e937b3f24f
tibbsm <tibbs.marc@gmail.com>
2019-01-17 14:26:05 +0900

    polishing python project

--- python/mypython.py
+++ python/mypython.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
+
+###############################################################################
+# Name:         Marc Tibbs
+# Email:        tibbsm@oregonstate.edu
+# Class:        CS344 - Winter 2019
+# Assignment:   Program Py
+# Due date:     Feb 27 2019
+###############################################################################
+
 import random
 import string
 
 def main():
-	
-	for i in range(1,4):
-		filename="testfile" + str(i)
-		f = open(filename, "w+")
 
+	# Create 3 files named testfile[x] where x is its order number	
+	for i in range(1,4):
+		f = open("testfile" + str(i), "w+")
+
+		# Create a random, 10-letter string, print it, and write it to file
 		randoms=(''.join(random.choice(string.ascii_lowercase) for x in range(10)))	
 		print(randoms)
 		f.write(randoms+"\n")
 
-	num1=random.randint(1, 42)
-	num2=random.randint(1, 42)
-	print(num1)
-	print(num2)
-	print(num1 * num2)
+	# Randomly generate 2 numbers print them and the product
+	num1, num2=random.randint(1, 42), random.randint(1, 42)
+	print(num1,"\n", num2,"\n",(num1 * num2), sep='')
 
 if __name__ == "__main__":
     main()
